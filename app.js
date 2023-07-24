19b48299179d3211c3b2d28be326acca46683360
Leslie Orellana <lesore0789@gmail.com>
2023-07-24 03:42:23 -0700

    Connected Mongoose and made sure db is working

--- app.js
+++ app.js
@@ -1,8 +1,29 @@
 const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const BlogPost = require('./models/blogpost');
+
+mongoose.connect('mongodb://127.0.0.1:27017/codingblog');
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+  console.log("Database connected")
+})
+
 const app = express();
 
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
+
 app.get('/', (req, res) => {
-  res.send('Hello!!!')
+  res.render('home')
+})
+app.get('/makeblogpost', async (req, res) => {
+  const post = new BlogPost({title: 'For Loops', subTitle: "And all the other loops"});
+  await post.save();
+  res.send(post)
 })
 
 app.listen(3000, () => {
