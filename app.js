a73e2a93560e1ac4a48fe99c6bbd51ba901cabeb
Leslie Orellana <lesore0789@gmail.com>
2023-08-09 04:24:22 -0700

    Restructured comment routes with Router

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const BlogPost = require('./models/blogpost');
 const Comment = require('./models/comment.js');
 
 const blogposts = require('./routes/blogposts.js')
+const comments = require('./routes/comments.js')
 
 mongoose.connect('mongodb://127.0.0.1:27017/codingblog');
 
@@ -30,40 +31,13 @@ app.use(express.urlencoded({extended: true}))
 app.use(methodOverride('_method'));
 
 
-const validateComment = (req, res, next) => {
-  const { error } = commentSchema.validate(req.body);
-  if(error) {
-    const msg = error.details.map(el => el.message).join(',');
-    throw new ExpressError(msg, 400)
-  } else {
-    next();
-  }
-}
-
-app.use('/blogposts', blogposts)
+app.use('/blogposts', blogposts);
+app.use('/blogposts/:id/comments', comments)
 
 // HOME
 app.get('/', (req, res) => {
   res.render('home')
 })
-
-
-// Comment Routes
-app.post('/blogposts/:id/comments', validateComment, catchAsync(async (req, res) => {
-  const blogpost = await BlogPost.findById(req.params.id);
-  const comment = new Comment(req.body.comment);
-  blogpost.comments.push(comment);
-  await comment.save();
-  await blogpost.save();
-  res.redirect(`/blogposts/${blogpost._id}`)
-}))
-
-app.delete('/blogposts/:id/comments/:commentId', catchAsync(async (req, res) => {
-  const { id, commentId } = req.params;
-  await BlogPost.findByIdAndUpdate(id, {$pull: {comments: commentId}})
-  await Comment.findByIdAndDelete(commentId);
-  res.redirect(`/blogposts/${id}`);
-}))
 
 app.all('*', (req, res, next) => {
   next(new ExpressError('Page Not Found', 404))
---
+++ routes/comments.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const router = express.Router({mergeParams: true});
+const catchAsync = require('../utils/catchAsync');
+const ExpressError = require('../utils/ExpressError');
+const BlogPost = require('../models/blogpost');
+const Comment = require('../models/comment');
+const { commentSchema } = require('../schemas.js')
+
+const validateComment = (req, res, next) => {
+  const { error } = commentSchema.validate(req.body);
+  if(error) {
+    const msg = error.details.map(el => el.message).join(',');
+    throw new ExpressError(msg, 400)
+  } else {
+    next();
+  }
+}
+
+// Comment Routes
+router.post('/', validateComment, catchAsync(async (req, res) => {
+  const blogpost = await BlogPost.findById(req.params.id);
+  const comment = new Comment(req.body.comment);
+  blogpost.comments.push(comment);
+  await comment.save();
+  await blogpost.save();
+  res.redirect(`/blogposts/${blogpost._id}`)
+}))
+
+router.delete('/:commentId', catchAsync(async (req, res) => {
+  const { id, commentId } = req.params;
+  await BlogPost.findByIdAndUpdate(id, {$pull: {comments: commentId}})
+  await Comment.findByIdAndDelete(commentId);
+  res.redirect(`/blogposts/${id}`);
+}))
+
+module.exports = router;
