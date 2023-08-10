897bfc867304c6b96160216daab7f388b8405e8a
Leslie Orellana <lesore0789@gmail.com>
2023-08-10 03:16:08 -0700

    Installed sessions and config

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
+const session = require('express-session');
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
 
@@ -28,7 +29,19 @@ app.use(express.urlencoded({extended: true}))
 app.use(methodOverride('_method'));
 
 // Tells Express to serve our public directory
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
+
+const sessionConfig = {
+  secret: 'thisshouldbeasecret',
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    httpOnly: true,
+    expires: Date.now() + 1000 * 60 * 60 *24 * 7,
+    maxAge: 1000 * 60 * 60 *24 * 7
+  }
+}
+app.use(session(sessionConfig))
 
 
 app.use('/blogposts', blogposts);
