28027c0791ae30532b1adc7aaa7a79a5f70371f5
Leslie Orellana <lesore0789@gmail.com>
2023-08-11 02:55:34 -0700

    Configure passport on app

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const session = require('express-session');
 const flash = require('connect-flash');
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user')
 
 
 const blogposts = require('./routes/blogposts.js')
@@ -52,6 +55,13 @@ app.use((req, res, next) => {
   res.locals.error = req.flash('error')
   next();
 })
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.use('/blogposts', blogposts);
 app.use('/blogposts/:id/comments', comments)
