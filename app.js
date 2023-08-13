28f4fa8499e57c6ea7fa9e3c32754e0d2e35b1aa
Leslie Orellana <lesore0789@gmail.com>
2023-08-13 02:47:31 -0700

    Logic added, Login link if logged out and vice versa

--- app.js
+++ app.js
@@ -49,19 +49,21 @@ app.use(session(sessionConfig))
 
 app.use(flash());
 
-// Flash Middleware
-app.use((req, res, next) => {
-  res.locals.success = req.flash('success');
-  res.locals.error = req.flash('error')
-  next();
-})
-
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
 
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
+
+// Flash Middleware
+app.use((req, res, next) => {
+  res.locals.currentUser = req.user;
+  res.locals.success = req.flash('success');
+  res.locals.error = req.flash('error')
+  next();
+})
+
 
 app.use('/', userRoutes);
 app.use('/blogposts', blogpostRoutes);
