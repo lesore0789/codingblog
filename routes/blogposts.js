8478a842e16a3700e04f1d6ef1eea7ad79afcc3a
Leslie Orellana <lesore0789@gmail.com>
2023-08-18 23:13:57 -0700

    New branch to start where I last had working code

--- routes/blogposts.js
+++ routes/blogposts.js
@@ -3,11 +3,17 @@ const router = express.Router();
 const blogposts = require('../controllers/blogposts')
 const catchAsync = require('../utils/catchAsync');
 const {isLoggedIn, validateBlogPost, isAuthor} = require('../middleware');
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
 
 // All BlogPosts - Index Page, Submitting the new Post
 router.route('/')
   .get(catchAsync(blogposts.index))
-  .post(isLoggedIn, validateBlogPost, catchAsync(blogposts.createBlogPost))
+  // .post(isLoggedIn, validateBlogPost, catchAsync(blogposts.createBlogPost))
+  .post(upload.array('image'),(req, res) => {
+    console.log(req.body, req.files);
+    res.send('It worked?')
+  })
 
 // Create a new post
 router.get('/new', isLoggedIn, blogposts.renderNewForm)
