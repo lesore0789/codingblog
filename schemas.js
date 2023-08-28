a61367b627d918e0b15ff195f6b58f71d0a0b6f9
Leslie Orellana <lesore0789@gmail.com>
2023-08-27 19:34:52 -0700

    Added additional HTML sanitation

--- schemas.js
+++ schemas.js
@@ -1,17 +1,40 @@
-const Joi = require('joi');
+const BaseJoi = require('joi');
+const sanitizeHtml = require('sanitize-html');
+
+const extension = (joi) => ({
+    type: 'string',
+    base: joi.string(),
+    messages: {
+        'string.escapeHTML': '{{#label}} must not include HTML!'
+    },
+    rules: {
+        escapeHTML: {
+            validate(value, helpers) {
+                const clean = sanitizeHtml(value, {
+                    allowedTags: [],
+                    allowedAttributes: {},
+                });
+                if (clean !== value) return helpers.error('string.escapeHTML', { value })
+                return clean;
+            }
+        }
+    }
+});
+
+const Joi = BaseJoi.extend(extension)
 
 module.exports.blogPostSchema = Joi.object({
   blogpost: Joi.object({
-    title: Joi.string().required(),
+    title: Joi.string().required().escapeHTML(),
     // image: Joi.string().required(),
-    body: Joi.string().required()
+    body: Joi.string().required().escapeHTML()
   }).required(),
   deleteImages: Joi.array()
 });
 
 module.exports.commentSchema = Joi.object({
   comment: Joi.object({
-    body: Joi.string().required(),
+    body: Joi.string().required().escapeHTML(),
     rating: Joi.number().required()
   }).required()
 });
